c59e4a59b3a1277d50ce2630d039dbfebe3d3f40
Arash Zandi <zandiarash@hotmail.com>
2025-04-13 10:51:09 +0330

    BlazorApp for new tests has been added

--- Test/AspireAppTest.AppHost/Program.cs
+++ Test/AspireAppTest.AppHost/Program.cs
@@ -2,11 +2,15 @@ using Arshid.Aspire.ApiDocs.Extensions;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.AspireAppTest_ApiService>("apiservice")
+var ApiService = builder.AddProject<Projects.AspireAppTest_ApiService>("ApiService")
     .WithScalar()
     .WithSwagger()
-    .WithOpenApi()
-    .WithCustomUrl("https://127.0.0.1:5000/CustomRoute/CustomPage1")
-    .WithRoute("/CustomRoute/CustomPage2"); //The URL become something like this {protocol}://{url}:{port}/CustomRoute/CustomPage2
+    .WithOpenApi();
+
+var BlazorApp = builder.AddProject<Projects.AspireAppTest_BlazorApp>("BlazorApp")
+    .WithCustomUrl("http://127.0.0.1:5001/CustomRoute/CustomPage1")
+    .WithCustomUrl("http://127.0.0.1:5001/Counter")
+    .WithRoute("/Counter") //The URL become something like this {protocol}://{url}:{port}/CustomRoute/.../CustomPage2
+    .WithRoute("/CustomRoute/CustomPage"); //The URL become something like this {protocol}://{url}:{port}/CustomRoute/.../CustomPage2
 
 builder.Build().Run();
